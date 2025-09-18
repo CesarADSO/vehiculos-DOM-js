25f2d3fd78f3531bfbfd662793083500ee8726d8
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-18 17:40:56 -0500

    Comenzé a realizar el código JavaScript para la manipulación del DOM

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form-vehiculo');
+
+form.addEventListener('submit', (e) => {
+
+
+    e.preventDefault();
+
+    const valorFoto = document.getElementById('foto-input').value;
+
+})
